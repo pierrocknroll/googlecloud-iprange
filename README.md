26dd28b1e39ce5e36df8bd5fe70fc6321229b28e
Pierre Martel <pierrocknroll@yahoo.fr>
2019-12-05 16:55:20 +0100

    Add files

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # googlecloud-iprange
+
+List all IP ranges from Google Cloud Platform.
+
+You can execute the bash script or grab directly the list
+
+I will update this list as soon as I can.
+
+See :
+https://cloud.google.com/compute/docs/faq#find_ip_range
---
+++ list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+get_gcp_ip_ranges() {
+  dig @8.8.8.8 +short txt "$1" | tr ' ' '\n' |
+    while read -r line; do
+      case "$line" in
+        include:*)
+          get_gcp_ip_ranges "${line#*:}"
+          ;;
+        ip4:*)
+          echo "${line#*:}"
+          ;;
+      esac
+  done
+}
+
+get_gcp_ip_ranges "_cloud-netblocks.googleusercontent.com"
